877104ea8dc694d8238ea0ec2b04cca728a559d6
unknown <sarthak.madan17@gmail.com>
2018-08-26 17:33:59 -0700

    now working?

--- static/game.js
+++ static/game.js
@@ -25,17 +25,12 @@ function start(){
     height: window.innerHeight || document.body.clientHeight
   }
 
-
   var movement = {
     up: false,
     down: false,
     left: false,
     right: false
   }
-
-  mouse.x = (event.clientX-size.width/2)/zoom_val;
-  mouse.y = (event.clientY-size.height/2)/zoom_val;
-}
 
   var side = 40;
   var height = (Math.sqrt(3)*side)/2;
@@ -77,7 +72,6 @@ function start(){
     }
     return arr1;
   }
-
 
   socket.on('message', function(data) {
     console.log(data);
@@ -127,16 +121,6 @@ function start(){
   });
 
 
-  mouse.x = (event.clientX-size.width/2)/zoom_val;
-  mouse.y = (event.clientY-size.height/2)/zoom_val;
-}
-
-document.onmousemove = function (event) {
-  mouse.x = (event.clientX-size.width/2)/zoom_val;
-  mouse.y = (event.clientY-size.height/2)/zoom_val;
-}
-
-
   document.onmousedown = function(event){
     mouse.mouseDown = true;
   }
@@ -170,37 +154,10 @@ document.onmousemove = function (event) {
   var canvas = document.getElementById('canvas');
   canvas.width = size.width;
   canvas.height = size.height;
-
   var ctx = canvas.getContext('2d');
   socket.on('state', function(players) {
     if(id==null) {
       return;
-
-
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, size.width, size.height);
-
-  xdif = (size.width/zoom_val)/2-player.x;
-  ydif = (size.height/zoom_val)/2-player.y;
-
-  ctx.save();
-  ctx.scale(zoom_val,zoom_val);
-  drawGraph(50, zoom_val);
-  drawPlayers(players);
-  drawBullets(players);
-
-  ctx.restore();
-});
-
-function drawGraph(dist, zoom_val) {
-  ctx.strokeStyle = "#323232";
-  var i;
-  for(i = xdif%50; i<size.width/zoom_val; i+=dist) {
-    var loc = i-xdif;
-    if(loc>=0 && loc<=gameSize.x) {
-      var y1 = Math.max(0, ydif);
-      var y2 = Math.min(size.height/zoom_val, gameSize.y+ydif);
-      drawLine(i, y1, i, y2, 5, "#323232");
     }
     var player = players[id];
     zoom_val=1.5/player.zoom;
@@ -271,11 +228,9 @@ function drawGraph(dist, zoom_val) {
           }
           drawTriangle(player, triangle.x+player.x, triangle.y+player.y, triangle.dir, color);
         }
-        drawTriangle(triangle.x+player.x, triangle.y+player.y, triangle.dir, triangle.height, color);
       }
     }
   }
-
 
   function drawBullets(players) {
     for(i in players) {
@@ -283,26 +238,13 @@ function drawGraph(dist, zoom_val) {
         var triangle = players[i].bullets[j];
         drawTriangle(players[i], triangle.x, triangle.y, triangle.dir, "#ab3c3c");
       }
-
-function drawBullets(players) {
-  for(i in players) {
-    for (j in players[i].bullets) {
-      var triangle = players[i].bullets[j];
-      drawTriangle(triangle.x, triangle.y, triangle.dir, triangle.height, "#ab3c3c");
-
     }
   }
-
 
   function drawTriangle(player, x, y, dir, color) {
     ctx.lineWidth = 2;
     ctx.strokeStyle = 'black';
     ctx.fillStyle = color;
-
-function drawTriangle(x, y, dir, height, color) {
-  ctx.lineWidth = 2;
-  ctx.strokeStyle = 'black';
-  ctx.fillStyle = color;
 
     var xcord = {};
     var ycord = {};
